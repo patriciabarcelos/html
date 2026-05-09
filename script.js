4cad044ca28a2dce21293a03ae5da66708911d3e
patriciabarcelos <patriciabarcelos6@hotmail.com>
2026-05-09 16:40:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Carrinho </title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header>
+    <nav>
+        <a href="index.html">Home</a>
+        <a href="produtos.html">Produtos</a>
+        <a href="carrinho.html"> 🛒 Carrinho (<span id="contador-carrinho">0</span>)</a>
+    </nav>
+</header>
+
+<h1>Meu Carrinho 🛒</h1>
+
+
+<section id="lista-carrinho"></section>
+
+<h2>Total: R$ <span id="total">0.00</span></h2>
+
+<button>Finalizar Pedido</button>
+
+<script src="script.js"></script>
+</body>
+</html>
